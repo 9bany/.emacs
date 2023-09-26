c56f5138be1a7dbdd2c9a68bc43bbea203893ee2
9bany <bany.y0599@gmail.com>
2023-09-26 13:23:26 +0700

    fixing some bugs

--- init.el
+++ init.el
@@ -549,6 +549,14 @@ Young Fingaprint   ;; Replace list hyphen with dot
 
 (require 'go-projectile)
 
+(require 'protobuf-mode)
+(defconst my-protobuf-style
+   '((c-basic-offset . 2)
+     (indent-tabs-mode . nil)))
+
+ (add-hook 'protobuf-mode-hook
+   (lambda () (c-add-style "my-style" my-protobuf-style t)))
+
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
